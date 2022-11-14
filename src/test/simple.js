9a5c8e40630e0642a167de6883b71021e9351069
Oskar Hane <oh@oskarhane.com>
2022-11-14 11:09:19 +0100

    Make first test pass

--- src/test/simple.js
+++ src/test/simple.js
@@ -1,14 +1,32 @@
-import { equal } from "node:assert";
-import { validateSchema } from "..";
-import { readFile } from "../fs.utils";
+import { strict as assert } from "node:assert";
+import { fileURLToPath } from "url";
+import path from "path";
+import { validateSchema } from "../index.js";
+import { readFile } from "../fs.utils.js";
 
-const JSON_SCHEMA_FILE = path.resolve(__dirname, "graphDescriptionNew.json");
+const __dirname = path.dirname(fileURLToPath(new URL(import.meta.url)));
+const JSON_SCHEMA_FILE = path.resolve(__dirname, "../graphDescriptionNew.json");
 const JSON_SCHEMA = readFile(JSON_SCHEMA_FILE);
 
-describe("Simple hally path tests", () => {
-    test("Minimal test", () => {
-        const minimalGraphSchema = readFile("./test-schemas/minial.json");
+function describe(name, cb) {
+    console.log(name);
+    cb();
+}
+function test(name, cb) {
+    try {
+        cb();
+        console.log(`${name} PASSED`);
+    } catch (e) {
+        console.error(`${name} FAILED`, e);
+    }
+}
 
+describe("Simple happy path tests", () => {
+    test("Minimal test", () => {
+        const minimalGraphSchema = readFile(
+            path.resolve(__dirname, "./test-schemas/minimal.json")
+        );
         const valid = validateSchema(JSON_SCHEMA, minimalGraphSchema);
+        assert.strictEqual(valid, true);
     });
 });
