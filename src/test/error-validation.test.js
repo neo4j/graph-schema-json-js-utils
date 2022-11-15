9660c795148fd219203d3601a720d4d49bc312b2
Mewken <56892754+Mewken@users.noreply.github.com>
2022-11-15 11:04:10 +0100

    kommentar

--- src/test/error-validation.test.js
+++ src/test/error-validation.test.js
@@ -5,11 +5,14 @@ import { SchemaValidationError, validateSchema } from "../index.js";
 import { readFile } from "../fs.utils.js";
 import { describe, test } from "./utils/helpers.utils.js";
 
+
 const __dirname = path.dirname(fileURLToPath(new URL(import.meta.url)));
 const JSON_SCHEMA_FILE = path.resolve(__dirname, "../graphDescriptionNew.json");
 const JSON_SCHEMA = readFile(JSON_SCHEMA_FILE);
 
+// Validate type errors == schemas we expect NOT to pass
 describe("Validate type errors", () => {
+
     test("Identifies unsupported types", () => {
         const unsupportedDataTypeSchema = readFile(
             path.resolve(__dirname, "./test-schemas/unsupported-data-type.json")
