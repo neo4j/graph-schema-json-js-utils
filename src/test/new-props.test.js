025eb1cff0412affb4b1e0564bd601ef4ebf5df7
Oskar Hane <oh@oskarhane.com>
2022-12-12 12:23:27 +0100

    Add support for mixed property types

--- src/test/new-props.test.js
+++ src/test/new-props.test.js
@@ -10,11 +10,11 @@ const JSON_SCHEMA_FILE = path.resolve(__dirname, "../json-schema.json");
 const JSON_SCHEMA = readFile(JSON_SCHEMA_FILE);
 
 describe("Props", () => {
-    test("New pros", () => {
-        const dbSchema = readFile(
-            path.resolve(__dirname, "./test-schemas/new-properties-format.json")
-        );
-        const valid = validateSchema(JSON_SCHEMA, dbSchema);
-        assert.strictEqual(valid, true);
-    });
+  test("New pros", () => {
+    const dbSchema = readFile(
+      path.resolve(__dirname, "./test-schemas/new-properties-format.json")
+    );
+    const valid = validateSchema(JSON_SCHEMA, dbSchema);
+    assert.strictEqual(valid, true);
+  });
 });
