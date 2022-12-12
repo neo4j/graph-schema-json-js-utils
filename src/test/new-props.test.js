7df2fab54feb8dffc13d38ff90de624cd40df00b
Oskar Hane <oh@oskarhane.com>
2022-12-12 11:47:44 +0100

    Support array of new property types

---
+++ src/test/new-props.test.js
@@ -0,0 +1,20 @@
+import { strict as assert } from "node:assert";
+import { fileURLToPath } from "url";
+import path from "path";
+import { validateSchema } from "../index.js";
+import { readFile } from "../fs.utils.js";
+import { describe, test } from "vitest";
+
+const __dirname = path.dirname(fileURLToPath(new URL(import.meta.url)));
+const JSON_SCHEMA_FILE = path.resolve(__dirname, "../json-schema.json");
+const JSON_SCHEMA = readFile(JSON_SCHEMA_FILE);
+
+describe("Props", () => {
+    test("New pros", () => {
+        const dbSchema = readFile(
+            path.resolve(__dirname, "./test-schemas/new-properties-format.json")
+        );
+        const valid = validateSchema(JSON_SCHEMA, dbSchema);
+        assert.strictEqual(valid, true);
+    });
+});
