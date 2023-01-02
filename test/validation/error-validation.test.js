4adebce8c06a1b266eef8935264e35016f084b22
Oskar Hane <oh@oskarhane.com>
2023-01-02 15:44:10 +0100

    Move validation test files into separate folder

--- test/validation/error-validation.test.js
+++ test/validation/error-validation.test.js
@@ -1,12 +1,12 @@
 import { strict as assert } from "node:assert";
 import { fileURLToPath } from "url";
 import path from "path";
-import { SchemaValidationError, validateSchema } from "../src/index.js";
-import { readFile } from "./fs.utils.js";
+import { SchemaValidationError, validateSchema } from "../../src/index.js";
+import { readFile } from "../fs.utils.js";
 import { describe, test } from "vitest";
 
 const __dirname = path.dirname(fileURLToPath(new URL(import.meta.url)));
-const JSON_SCHEMA_FILE = path.resolve(__dirname, "../json-schema.json");
+const JSON_SCHEMA_FILE = path.resolve(__dirname, "../../json-schema.json");
 const JSON_SCHEMA = readFile(JSON_SCHEMA_FILE);
 
 // Validate type errors == schemas we expect NOT to pass
--- test/validation/simple.test.js
+++ test/validation/simple.test.js
@@ -1,12 +1,12 @@
 import { strict as assert } from "node:assert";
 import { fileURLToPath } from "url";
 import path from "path";
-import { validateSchema } from "../src/index.js";
-import { readFile } from "./fs.utils.js";
+import { validateSchema } from "../../src/index.js";
+import { readFile } from "../fs.utils.js";
 import { describe, test } from "vitest";
 
 const __dirname = path.dirname(fileURLToPath(new URL(import.meta.url)));
-const JSON_SCHEMA_FILE = path.resolve(__dirname, "../json-schema.json");
+const JSON_SCHEMA_FILE = path.resolve(__dirname, "../../json-schema.json");
 const JSON_SCHEMA = readFile(JSON_SCHEMA_FILE);
 
 // Happy path == schemas we expect to pass
