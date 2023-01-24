490e3c5d64fe80350477d6c8ef2c2bca8eadb595
Mewken <56892754+Mewken@users.noreply.github.com>
2023-01-24 14:01:04 +0100

    completed addition of test for graphSchema

--- test/validation/validation.test.js
+++ test/validation/validation.test.js
@@ -11,21 +11,27 @@ const JSON_SCHEMA_FILE = path.resolve(__dirname, "../../json-schema.json");
 const JSON_SCHEMA = readFile(JSON_SCHEMA_FILE);
 
 describe("Validate if JSON-documen is a string", () => {
+
+  const  NON_STRING = 11;
+  const NON_JSON = "}"
+
   test("Identifies if JSON schema is a string and is parseable to JSON", () => {
     const fullSchema = readFile(
       path.resolve(__dirname, "./test-schemas/full.json")
     );
 
-    assert.throws(() => validateSchema(11, fullSchema), InputTypeError);
+    assert.throws(() => validateSchema(NON_STRING, fullSchema), InputTypeError);
+    assert.throws(
+      () => validateSchema(NON_JSON, fullSchema), InputTypeError);
   });
 
   test("Identifies if graph schema is a string and is parseble JSON", () => {
-    const exampleGraphSchema = readFile(
-      path.resolve(__dirname, "./test-schemas/example-graph-schema.json")
-    );
 
+    assert.throws(() => validateSchema(JSON_SCHEMA, NON_STRING), InputTypeError);
     assert.throws(
-      () => validateSchema("}", exampleGraphSchema), InputTypeError);
+      () => validateSchema(JSON_SCHEMA, NON_JSON), InputTypeError);
+
+    
   });
   
 });
