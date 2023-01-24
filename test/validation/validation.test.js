93d47788bef843a43986f8ec8e0381afc535e463
Mewken <56892754+Mewken@users.noreply.github.com>
2023-01-24 13:09:40 +0100

    Updated validation.test

--- test/validation/validation.test.js
+++ test/validation/validation.test.js
@@ -1,7 +1,7 @@
 import { strict as assert } from "node:assert";
 import { fileURLToPath } from "url";
 import path from "path";
-import { SchemaValidationError, validateSchema } from "../../src/index.js";
+import { validateSchema } from "../../src/index.js";
 import { readFile } from "../fs.utils.js";
 import { describe, test } from "vitest";
 import { InputTypeError } from "../../src/validation.js";
@@ -10,21 +10,22 @@ const __dirname = path.dirname(fileURLToPath(new URL(import.meta.url)));
 const JSON_SCHEMA_FILE = path.resolve(__dirname, "../../json-schema.json");
 const JSON_SCHEMA = readFile(JSON_SCHEMA_FILE);
 
-describe("Validate if string i JSON-document", () => {
-  test("Identifies if JSON schema is a string and is parseble JSON", () => {
+describe("Validate if JSON-documen is a string", () => {
+  test("Identifies if JSON schema is a string and is parseable to JSON", () => {
     const fullSchema = readFile(
       path.resolve(__dirname, "./test-schemas/full.json")
     );
 
     assert.throws(() => validateSchema(11, fullSchema), InputTypeError);
+  });
 
+  test("Identifies if graph schema is a string and is parseble JSON", () => {
     const exampleGraphSchema = readFile(
       path.resolve(__dirname, "./test-schemas/example-graph-schema.json")
     );
 
     assert.throws(
-      () => validateSchema("}", exampleGraphSchema),
-      InputTypeError
-    );
+      () => validateSchema("}", exampleGraphSchema), InputTypeError);
   });
+  
 });
