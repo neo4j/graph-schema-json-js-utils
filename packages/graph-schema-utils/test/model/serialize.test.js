607ecbe969e494111df18aead339a7fb14a549f1
Oskar Hane <oh@oskarhane.com>
2023-02-06 10:23:02 +0100

    Validate serialized schema in test

--- packages/graph-schema-utils/test/model/serialize.test.js
+++ packages/graph-schema-utils/test/model/serialize.test.js
@@ -3,19 +3,33 @@ import { fileURLToPath } from "url";
 import path from "path";
 import { readFile } from "../fs.utils.js";
 import { describe, test } from "vitest";
-import { model } from "../../src";
+import { model, validateSchema } from "../../src";
 
 const __dirname = path.dirname(fileURLToPath(new URL(import.meta.url)));
+const JSON_SCHEMA_FILE = path.resolve(
+  __dirname,
+  "../../../json-schema/json-schema.json"
+);
+const JSON_SCHEMA = readFile(JSON_SCHEMA_FILE);
 
 describe("Serializer tests", () => {
   const fullSchema = readFile(
     path.resolve(__dirname, "./test-schemas/full.json")
   );
+
+  test("Serialized schema can be validated", () => {
+    const parsed = model.GraphSchemaRepresentation.parseJson(fullSchema);
+    const serialized = parsed.toJson();
+    const validation = validateSchema(serialized, JSON_SCHEMA);
+    assert.strictEqual(validation, true);
+  });
+
   test("Can parse a graph schema and serialize it to be the same", () => {
     const parsed = model.GraphSchemaRepresentation.parseJson(fullSchema);
     const serialized = parsed.toJson();
     assert.deepEqual(JSON.parse(serialized), JSON.parse(fullSchema));
   });
+
   test("Throws if label refs aren't connected", () => {
     const parsed = model.GraphSchemaRepresentation.parseJson(fullSchema);
     parsed.graphSchema.nodeObjectTypes[0].labels[0] = undefined;
@@ -24,6 +38,7 @@ describe("Serializer tests", () => {
       new Error("Not all labels are defined")
     );
   });
+
   test("Throws if type ref aren't connected", () => {
     const parsed = model.GraphSchemaRepresentation.parseJson(fullSchema);
     parsed.graphSchema.relationshipObjectTypes[0].type = undefined;
@@ -32,6 +47,7 @@ describe("Serializer tests", () => {
       new Error("RelationshipObjectType.type is not defined")
     );
   });
+
   test("Throws if from ref aren't connected", () => {
     const parsed = model.GraphSchemaRepresentation.parseJson(fullSchema);
     parsed.graphSchema.relationshipObjectTypes[0].from = undefined;
@@ -40,6 +56,7 @@ describe("Serializer tests", () => {
       new Error("RelationshipObjectType.from is not defined")
     );
   });
+
   test("Throws if to ref aren't connected", () => {
     const parsed = model.GraphSchemaRepresentation.parseJson(fullSchema);
     parsed.graphSchema.relationshipObjectTypes[0].to = undefined;
