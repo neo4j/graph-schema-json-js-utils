93c86f3864506f9b7716e16d44d74b5cac929c69
Oskar Hane <oh@oskarhane.com>
2023-02-06 10:03:35 +0100

    Fix serialized tests for broken objects

--- packages/graph-schema-utils/test/model/serialize.test.js
+++ packages/graph-schema-utils/test/model/serialize.test.js
@@ -8,49 +8,41 @@ import { model } from "../../src";
 const __dirname = path.dirname(fileURLToPath(new URL(import.meta.url)));
 
 describe("Serializer tests", () => {
+  const fullSchema = readFile(
+    path.resolve(__dirname, "./test-schemas/full.json")
+  );
   test("Can parse a graph schema and serialize it to be the same", () => {
-    const fullSchema = readFile(
-      path.resolve(__dirname, "./test-schemas/full.json")
-    );
     const parsed = model.GraphSchemaRepresentation.parseJson(fullSchema);
     const serialized = parsed.toJson();
     assert.deepEqual(JSON.parse(serialized), JSON.parse(fullSchema));
   });
   test("Throws if label refs aren't connected", () => {
-    const brokenSchema = readFile(
-      path.resolve(__dirname, "./test-schemas/broken-label.json")
-    );
-    const parsed = model.GraphSchemaRepresentation.parseJson(brokenSchema);
+    const parsed = model.GraphSchemaRepresentation.parseJson(fullSchema);
+    parsed.graphSchema.nodeObjectTypes[0].labels[0] = undefined;
     assert.throws(
       () => parsed.toJson(),
       new Error("Not all labels are defined")
     );
   });
   test("Throws if type ref aren't connected", () => {
-    const brokenSchema = readFile(
-      path.resolve(__dirname, "./test-schemas/broken-type.json")
-    );
-    const parsed = model.GraphSchemaRepresentation.parseJson(brokenSchema);
+    const parsed = model.GraphSchemaRepresentation.parseJson(fullSchema);
+    parsed.graphSchema.relationshipObjectTypes[0].type = undefined;
     assert.throws(
       () => parsed.toJson(),
       new Error("RelationshipObjectType.type is not defined")
     );
   });
   test("Throws if from ref aren't connected", () => {
-    const brokenSchema = readFile(
-      path.resolve(__dirname, "./test-schemas/broken-from.json")
-    );
-    const parsed = model.GraphSchemaRepresentation.parseJson(brokenSchema);
+    const parsed = model.GraphSchemaRepresentation.parseJson(fullSchema);
+    parsed.graphSchema.relationshipObjectTypes[0].from = undefined;
     assert.throws(
       () => parsed.toJson(),
       new Error("RelationshipObjectType.from is not defined")
     );
   });
   test("Throws if to ref aren't connected", () => {
-    const brokenSchema = readFile(
-      path.resolve(__dirname, "./test-schemas/broken-to.json")
-    );
-    const parsed = model.GraphSchemaRepresentation.parseJson(brokenSchema);
+    const parsed = model.GraphSchemaRepresentation.parseJson(fullSchema);
+    parsed.graphSchema.relationshipObjectTypes[0].to = undefined;
     assert.throws(
       () => parsed.toJson(),
       new Error("RelationshipObjectType.to is not defined")
