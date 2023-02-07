4c63c93e9266d3bb4419107ca592859bafc54e44
Oskar Hane <oh@oskarhane.com>
2023-02-07 10:10:28 +0100

    Rely on workspace packages rather than direct file imports
    It also seem slike `__dirname` works again.

--- packages/graph-schema-utils/test/model/parse.test.js
+++ packages/graph-schema-utils/test/model/parse.test.js
@@ -1,11 +1,8 @@
 import { strict as assert } from "node:assert";
-import { fileURLToPath } from "url";
 import path from "path";
 import { readFile } from "../fs.utils.js";
 import { describe, test } from "vitest";
 import { model } from "../../src";
-
-const __dirname = path.dirname(fileURLToPath(new URL(import.meta.url)));
 
 describe("Parser tests", () => {
   const fullSchema = readFile(
--- packages/graph-schema-utils/test/validation/validation.test.js
+++ packages/graph-schema-utils/test/validation/validation.test.js
@@ -5,13 +5,12 @@ import { validateSchema } from "../../src/index.js";
 import { readFile } from "../fs.utils.js";
 import { describe, test } from "vitest";
 import { InputTypeError } from "../../src/validation.js";
+import { createRequire } from "module";
 
-const __dirname = path.dirname(fileURLToPath(new URL(import.meta.url)));
-const JSON_SCHEMA_FILE = path.resolve(
-  __dirname,
-  "../../../json-schema/json-schema.json"
+const require = createRequire(import.meta.url);
+const JSON_SCHEMA = JSON.stringify(
+  require("@neo4j/graph-json-schema/json-schema.json")
 );
-const JSON_SCHEMA = readFile(JSON_SCHEMA_FILE);
 
 describe("Validate if JSON-documen is a string", () => {
   const NON_STRING = 11;
