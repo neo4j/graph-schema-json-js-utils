39d7ddf5c37a8c37749ac151f58e5b87f77b0f04
Oskar Hane <oh@oskarhane.com>
2023-12-21 14:29:15 +0100

    Fix types export from schema utils formatters

--- packages/graph-schema-utils/src/index.ts
+++ packages/graph-schema-utils/src/index.ts
@@ -1,6 +1,5 @@
 export { validateSchema, SchemaValidationError } from "./validation.js";
 import * as model from "./model/index.js";
-import * as json from "./formatters/json/index.js";
+import * as formatters from "./formatters/index.js";
 
-const formatters = { json };
 export { model, formatters };
