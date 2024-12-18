0317cf7b88e7a76c6c7ac33c00b7ee05dd3e3633
Junxiang Chen <chenfrey920417@outlook.com>
2024-12-18 15:05:01 +0000

    test: unit test on unit functions

--- packages/graph-schema-utils/src/formatters/json/extensions.test.ts
+++ packages/graph-schema-utils/src/formatters/json/extensions.test.ts
@@ -3,8 +3,46 @@ import { readFile } from "../../../test/fs.utils.js";
 import path from "path";
 import { fromJson } from "./index.js";
 import { strict as assert } from "node:assert";
+import {
+  hasDuplicateNodeLabelIds,
+  hasDuplicateNodeObjectTypeIds,
+} from "./extensions.js";
+import { RootSchemaJsonStruct } from "./types.js";
 
 describe("JSON formatter", () => {
+  describe("hasDuplicateNodeLabelIds", () => {
+    test("Identifies duplicated node labels", () => {
+      const schema = readFile(
+        path.resolve(__dirname, "./test-schemas/duplicated-nodeLabel-ids.json")
+      );
+      const schemaJson = JSON.parse(schema) as RootSchemaJsonStruct;
+
+      assert.strictEqual(
+        hasDuplicateNodeLabelIds(
+          schemaJson.graphSchemaRepresentation.graphSchema
+        ),
+        true
+      );
+    });
+  });
+  describe("hasDuplicateNodeObjectTypeIds", () => {
+    test("Identifies duplicated node object types", () => {
+      const schema = readFile(
+        path.resolve(
+          __dirname,
+          "./test-schemas/duplicated-nodeObjectType-ids.json"
+        )
+      );
+      const schemaJson = JSON.parse(schema) as RootSchemaJsonStruct;
+
+      assert.strictEqual(
+        hasDuplicateNodeObjectTypeIds(
+          schemaJson.graphSchemaRepresentation.graphSchema
+        ),
+        true
+      );
+    });
+  });
   describe("fromJson", () => {
     const schemaWithDuplicatedNodeLabels = readFile(
       path.resolve(__dirname, "./test-schemas/duplicated-nodeLabel-ids.json")
