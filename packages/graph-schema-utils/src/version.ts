3f817f3785d4e2fe8113313237bc57e86f7d7571
Patrik Gustafsson <patrik@acyclic.eu>
2026-03-12 11:22:19 +0100

    Tidy: enhance comments in version.ts for clarity on build variables

--- packages/graph-schema-utils/src/version.ts
+++ packages/graph-schema-utils/src/version.ts
@@ -1,3 +1,5 @@
-//Build variables update by packages/graph-schema-utils/set-build-info.cjs at build.
+//Build variables update by packages/graph-schema-utils/set-build-info.cjs mainly useful for development
+// PACKAGE_VERSION extracted from package.json to allow the dist to know and share its own version
 export const PACKAGE_VERSION = "";
+// LAST_BUILD_TIME set to now at build time to allow dist to know and share when it was built.
 export const LAST_BUILD_TIME = ""
