e8c9b8bf0ce1325689b62d4e5f6a55bc8fb2f546
GitHub Action Automation <team-nx@neo4j.com>
2026-05-06 11:32:13 +0000

    Version bump

--- packages/graph-schema-utils/CHANGELOG.md
+++ packages/graph-schema-utils/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @neo4j/graph-schema-utils
 
+## 1.0.0-next.20
+
+### Minor Changes
+
+- f5e9b55: Adding support for vector<integer8>, vector<integer16> and vector<integer32> types
+
 ## 1.0.0-next.19
 
 ### Minor Changes
--- packages/graph-schema-utils/src/version.ts
+++ packages/graph-schema-utils/src/version.ts
@@ -1,5 +1,5 @@
 //Build variables update by packages/graph-schema-utils/set-build-info.cjs mainly useful for development
 // PACKAGE_VERSION extracted from package.json to allow the dist to know and share its own version
-export const PACKAGE_VERSION = "1.0.0-next.18";
+export const PACKAGE_VERSION = "1.0.0-next.19";
 // LAST_BUILD_TIME set to now at build time to allow dist to know and share when it was built.
-export const LAST_BUILD_TIME = "2026-03-27T14:03:12.712Z"
+export const LAST_BUILD_TIME = "2026-05-06T11:32:09.205Z"
